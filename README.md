df5db8cf03abba0c0b229dbcea7745af878dd1c0
bendjen <unbolibobo@foxmail.com>
2018-10-22 21:29:39 +0800

    project init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# parcel-ts
-parcel项目迁移至typeScript
+#  本项目用于ts重写parcel项目
+
+#  以下为构建中遇到的问题记录
+
+# 1.create-react-app脚手架自定义配置webpack的方法
+
+  用 ./script 下的脚本将 ./config 中的配置动态插入默认的配置
+  用到的依赖有 "rewire"  "proxyquire"
+  参考链接：https://github.com/dceddia/create-react-app-customized
+
+# 2.typeScript中使用cssModule时引入声明
+
+  定义style.d.ts文件作声明
+  d
---
+++ config/config-overrides.dev.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = function(config) {
+  // Add the SASS loader second-to-last
+  // (last one must remain as the "file-loader")
+  let loaderList = config.module.rules[1].oneOf;
+  loaderList.splice(loaderList.length - 1, 0, {
+    test: /\.scss$/,
+    use: ["style-loader", "css-loader", "sass-loader"]
+  });
+}
